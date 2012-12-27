2cc31d4d717410c39e1d2d50e1e8e23d0650213a
Akshay Peshave <akshaypeshave.pun@gmail.com>
2012-12-27 11:44:18 -0500

    code commit 2

---
+++ GSC Catalog Suite/GSC Assimilator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Diagnostics;
+using System.IO;
+
+namespace GSC_Assimilator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\GSC_Decoded\\value_listing.txt", FileMode.Create);
+            int gscIndex = 0;
+            while (gscIndex <= 9537)
+            {
+                gscIndex++;
+                string gscIndexString="";
+                for (int gscIndexDigits = gscIndex.ToString().Length; gscIndexDigits < 5; gscIndexDigits++)
+                    gscIndexString += "0";
+                gscIndexString += gscIndex.ToString();
+                string command = @"C:\Catalogs\gsc.exe -g " + gscIndexString + " -n 2000000";
+                string output = "";
+
+                ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo = procStartInfo;
+                    proc.Start();
+
+                    output = proc.StandardOutput.ReadToEnd();
+                }
+                string[] valuesList = output.Split(' ');
+                
+                //byte[] valuesInByteArray = null;
+                int index = 0;
+                foreach (string value in valuesList)
+                {
+                    //valuesInByteArray[index] = Byte.Parse(value + "\n");
+                    //fs.WriteByte(Byte.Parse(value.to + "\n"));
+                    if (value == "")
+                        continue;
+
+
+                    string temp = value + " ";
+
+
+                    byte[] byteArray = System.Text.Encoding.Unicode.GetBytes(temp.ToCharArray());
+                    fs.Write(byteArray, 0, byteArray.Length);
+                }
+
+                //fs.Write(valuesInByteArray, 0, valuesInByteArray.Length);
+                System.Console.WriteLine(output);
+                
+            }
+            fs.Close();
+        }
+    }
+}
---
+++ GSC Catalog Suite/GSC Catalog Decoder/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.Common;
+using System.Data.Sql;
+using System.Data.ProviderBase;
+using System.Data.SqlClient;
+
+
+namespace GSC_Catalog_Decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //FileStream catalogFile = File.Open("D:\\Workspace\\CMSC 668\\Catalogs\\bincats_GSC_1.2\\N0000\\0001.gsc",FileMode.Open);
+
+            ////string gscString = Encoding.ASCII.GetString(bytesInCatalog);
+            ////System.Console.WriteLine(BitConverter.ToInt64(bytesInCatalog,0));
+
+            ////GSC Designation extraction Bytes 1-10 (10 digit integer)
+            //byte[] bytesInCatalog=new byte[10];
+            //catalogFile.Read(bytesInCatalog,0,10);
+            //System.Console.WriteLine(BitConverter.ToString(bytesInCatalog));
+
+            ////Right Ascension (in degrees) extraction Bytes 12-20 (9 digit-5 precision digits float)
+            //catalogFile.Seek(1, SeekOrigin.Current);
+            //bytesInCatalog = new byte[9];
+            //catalogFile.Read(bytesInCatalog, 0, 9);
+            //System.Console.WriteLine(BitConverter.ToDouble(bytesInCatalog,0));
+
+            ////Declination (in degrees) extraction Bytes 22-30 (9 digit-5 precision digits float)
+            //catalogFile.Seek(1, SeekOrigin.Current);
+            //bytesInCatalog = new byte[9];
+            //catalogFile.Read(bytesInCatalog,0,9);
+            //System.Console.WriteLine(BitConverter.ToDouble(bytesInCatalog, 0));
+
+            ////Mean position error (in arc second) extraction Bytes 33-36 (4 digit-1 precision digits float)
+            //catalogFile.Seek(2, SeekOrigin.Current);
+            //bytesInCatalog = new byte[8];
+            //bytesInCatalog=BitConverter.GetBytes(0);
+            //catalogFile.Read(bytesInCatalog, 4, 4);
+            //System.Console.WriteLine(BitConverter.ToDouble(bytesInCatalog, 0));
+
+            //System.Console.ReadKey();
+            //catalogFile.Close();
+        }
+    }
+}
